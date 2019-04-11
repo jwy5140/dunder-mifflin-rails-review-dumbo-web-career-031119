b17fdd942be8afcd0527d10afa7e808cdfed197c
Jeff Yu <jwy5140@gmail.com>
2019-04-11 11:05:13 -0400

    Index Weird

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,43 @@
 class EmployeesController < ApplicationController
+    def index
+        @employees = Employee.all
+    end
+
+    def show
+        @employee = Employee.find(params[:id])
+    end
+    
+    def new
+        @employee = Employee.new
+    end
+
+    def create
+        @employee = Employee.new(e_params(:first_name, :last_name, :alias, :title, :office, :dog_id))
+        if @employee.save
+            redirect_to employee_path(@employee)
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @employee = Employee.find(params[:id])
+    end
+
+    def update
+        @employee = Employee.find(params[:id])
+        if @employee.update!(e_params(:first_name, :last_name, :alias, :title, :office, :dog_id))
+            @employee.update(e_params(:first_name, :last_name, :alias, :title, :office, :dog_id))  
+            @employee.save
+            redirect_to employee_path(@employee)
+        else
+            render :edit
+        end
+    end
+
+    private
+
+    def e_params(*args)
+        params.require(:employee).permit(*args)
+    end
 end
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,7 @@
 class Dog < ApplicationRecord
+    has_many :employees
+
+    def owned_by
+        Employee.where(dog_id: self.id).count
+    end
 end
